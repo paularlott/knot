c9db1b0bea078929f50c5e2e1f36f84f56533370
Paul <paul@the7thdream.com>
2023-11-26 17:47:02 +0800

    Support srv+ for MySQL host

--- database/drivers/mysql/connect.go
+++ database/drivers/mysql/connect.go
@@ -3,9 +3,11 @@ package driver_mysql
 import (
 	"database/sql"
 	"fmt"
+	"strconv"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/paularlott/knot/util"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 )
@@ -17,12 +19,29 @@ type MySQLDriver struct{
 func (db *MySQLDriver) Connect() error {
   log.Debug().Msg("db: connecting to MySQL")
 
+  host := viper.GetString("server.mysql.host")
+  port := viper.GetInt("server.mysql.port")
+
+  // If the host starts with srv+ then lookup the SRV record
+  if viper.GetString("server.mysql.host")[:4] == "srv+" {
+    hostSrv, portSrv, err := util.GetTargetFromSRV(host[4:], viper.GetString("server.nameserver"))
+    if err != nil {
+      log.Fatal().Err(err).Msg("db: failed to lookup SRV record for MySQL database")
+    }
+
+    host = hostSrv
+    port, err = strconv.Atoi(portSrv)
+    if err != nil {
+      log.Fatal().Err(err).Msg("db: failed to convert MySQL port to integer")
+    }
+  }
+
   var err error
   db.connection, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
     viper.GetString("server.mysql.user"),
     viper.GetString("server.mysql.password"),
-    viper.GetString("server.mysql.host"),
-    viper.GetInt("server.mysql.port"),
+    host,
+    port,
     viper.GetString("server.mysql.database"),
   ))
   if err == nil {
