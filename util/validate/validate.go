bf7ad863504c2e7a0ddd07e6ac64b39c90decef7
Paul <paul@the7thdream.com>
2024-12-03 22:31:45 +0800

    Refactor validation regex

--- util/validate/validate.go
+++ util/validate/validate.go
@@ -6,50 +6,50 @@ import (
 )
 
 func Email(email string) bool {
-  re := regexp.MustCompile(`(?i)^([a-z0-9](?:[a-z0-9&'+=_\.-]+)?)@([a-z0-9_-]+)(\.[a-z0-9_-]+)*(\.[a-z]{2,})+$`)
-  return re.MatchString(email)
+	re := regexp.MustCompile(`(?i)^([a-z0-9](?:[a-z0-9&'+=_\.-]+)?)@([a-z0-9_-]+)(\.[a-z0-9_-]+)*(\.[a-z]{2,})+$`)
+	return re.MatchString(email)
 }
 
 func Uri(uri string) bool {
-  re := regexp.MustCompile(`(?i)^(srv\+)?https?:\/\/([\w-]+:[\w-]+@)?[\w-]+(?:\.[\w-]+)*(?::\d+)?(?:\/[\w ~\\\(\)%\=\.,\+-]*)*(?:\?[\w ~\\\(\)%\=\.,\+-]+=[\w ~\\\(\)%\=\.,\+-]*(?:&[\w ~\\\(\)%\=\.,\+-]+=[\w ~\\\(\)%\=\.,\+-]*)*)?(#[\w ~\\\(\)%\=\.,\/\+-]*)?$`)
-  return re.MatchString(uri)
+	re := regexp.MustCompile(`(?i)^(srv\+)?https?:\/\/([\w-]+:[\w-]+@)?[\w-]+(?:\.[\w-]+)*(?::\d+)?(?:\/(?:[\w~%()=.,+-]|%[0-9A-Fa-f]{2})*)*(?:\?(?:[\w~%()=.,+-]|%[0-9A-Fa-f]{2})+=(?:[\w~%()=.,+-]|%[0-9A-Fa-f]{2})*(?:&(?:[\w~%()=.,+-]|%[0-9A-Fa-f]{2})+=(?:[\w~%()=.,+-]|%[0-9A-Fa-f]{2})*)*)?(#[\w~%()=.,\/+-]*)?$`)
+	return re.MatchString(uri)
 }
 
 func Name(name string) bool {
-  re := regexp.MustCompile(`^[a-zA-Z][a-zA-Z0-9\-]{1,63}$`);
-  return re.MatchString(name) && !strings.Contains(name, "--");
+	re := regexp.MustCompile(`^[a-zA-Z][a-zA-Z0-9\-]{1,63}$`)
+	return re.MatchString(name) && !strings.Contains(name, "--")
 }
 
 func VarName(name string) bool {
-  re := regexp.MustCompile(`^[a-zA-Z][a-zA-Z0-9_]{1,63}$`);
-  return re.MatchString(name);
+	re := regexp.MustCompile(`^[a-zA-Z][a-zA-Z0-9_]{1,63}$`)
+	return re.MatchString(name)
 }
 
 func Password(password string) bool {
-  return len(password) >= 8
+	return len(password) >= 8
 }
 
 func TokenName(name string) bool {
-  return len(name) > 0 && len(name) <= 255
+	return len(name) > 0 && len(name) <= 255
 }
 
 func OneOf(value string, values []string) bool {
-  for _, v := range values {
-    if value == v {
-      return true
-    }
-  }
-  return false
+	for _, v := range values {
+		if value == v {
+			return true
+		}
+	}
+	return false
 }
 
 func Required(text string) bool {
-  return len(text) > 0
+	return len(text) > 0
 }
 
 func MaxLength(text string, length int) bool {
-  return len(text) <= length
+	return len(text) <= length
 }
 
 func IsNumber(value int, min int, max int) bool {
-  return value >= min && value <= max
+	return value >= min && value <= max
 }
