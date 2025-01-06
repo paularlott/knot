ab46ed01a2b1fdaa6b73e6da4a3923d97e0c59e9
Paul Arlott <paul@the7thdream.com>
2025-01-06 16:59:46 +0800

    Limit dns proxy workers

--- build/version.go
+++ build/version.go
@@ -2,7 +2,7 @@ package build
 
 var (
 	// Current version of knot
-	Version string = "0.12.3"
+	Version string = "0.12.4"
 
 	// The date the binary was built
 	Date string
--- internal/dnsproxy/cache.go
+++ internal/dnsproxy/cache.go
@@ -10,37 +10,58 @@ import (
 
 func (d *DNSProxy) maintainCache() {
 	go func() {
-		for {
-			time.Sleep(1 * time.Second)
+		ticker := time.NewTicker(1 * time.Second)
+		defer ticker.Stop()
+
+		// Channel for workers to listen on
+		requestChan := make(chan *dnsCacheEntry, 100)
+
+		// Start workers to refresh cache entries
+		for i := 0; i < 5; i++ {
+			go func() {
+				for entry := range requestChan {
+					log.Debug().Msgf("dnsproxy: worker refreshing cache entry %s IN %s", entry.request.Question[0].Name, dns.TypeToString[entry.request.Question[0].Qtype])
+					d.queryAndCache(entry.request, entry)
+				}
+			}()
+		}
+
+		for range ticker.C {
 
 			d.cacheMutex.Lock()
 			for k, v := range d.cache {
 
+				// If the TTL has reached the refresh threshold and entry has been used recently
+				// then refresh the entry in the background
+				if !v.refreshing && v.reply.Answer[0].Header().Ttl == BACKGROUND_REFRESH_AT && time.Since(v.lastAccess) < time.Duration(viper.GetUint16("dns.refresh_max_age"))*time.Second {
+					log.Debug().Msgf("dnsproxy: refresh cache entry %s IN %s", v.request.Question[0].Name, dns.TypeToString[v.request.Question[0].Qtype])
+
+					// Update the request Id
+					v.request.Id++
+					v.refreshing = true
+
+					// Send the request to the worker pool without blocking
+					select {
+					case requestChan <- v:
+					default:
+						log.Warn().Msgf("dnsproxy: worker pool is full, dropping request for %s IN %s", v.request.Question[0].Name, dns.TypeToString[v.request.Question[0].Qtype])
+
+						// Let the record expire
+						v.refreshing = false
+					}
+				}
+
 				// If the TTL is 0 then remove the entry unless it's being refreshed
 				if v.reply.Answer[0].Header().Ttl == 0 {
-					if v.refreshing {
-						log.Debug().Msgf("Cache entry %s IN %s is being refreshed keeping current record", v.request.Question[0].Name, dns.TypeToString[v.request.Question[0].Qtype])
-						continue
+					if !v.refreshing {
+						log.Debug().Msgf("dnsproxy: removing cache entry %s IN %s", v.request.Question[0].Name, dns.TypeToString[v.request.Question[0].Qtype])
+						delete(d.cache, k)
 					}
-
-					log.Debug().Msgf("Removing cache entry %s IN %s", v.request.Question[0].Name, dns.TypeToString[v.request.Question[0].Qtype])
-					delete(d.cache, k)
 				} else {
 					// Reduce the TTL of the entry by 1 second
 					for _, a := range v.reply.Answer {
 						a.Header().Ttl--
 					}
-				}
-
-				// If the TTL has reached the refresh threshold and entry has been used recently
-				// then refresh the entry in the background
-				if v.reply.Answer[0].Header().Ttl == BACKGROUND_REFRESH_AT && time.Since(v.lastAccess) < time.Duration(viper.GetUint16("dns.refresh_max_age"))*time.Second {
-					log.Debug().Msgf("Refresh cache entry %s IN %s", v.request.Question[0].Name, dns.TypeToString[v.request.Question[0].Qtype])
-
-					// Update the request Id
-					v.request.Id++
-					v.refreshing = true
-					go d.queryAndCache(v.request, v)
 				}
 			}
 			d.cacheMutex.Unlock()
