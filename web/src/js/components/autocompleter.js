13a0cf162ec981d923df0f43454698826c8ac848
Paul <paul@the7thdream.com>
2025-10-13 22:58:04 +0800

    UI improvements

--- web/src/js/components/autocompleter.js
+++ web/src/js/components/autocompleter.js
@@ -1,11 +1,50 @@
+const autocompleterBase = () => ({
+  search: '',
+  showList: false,
+  selectedIndex: -1,
+  handleKeydown(e) {
+    if (!this.showList) return;
+    const filtered = this.filteredOptions;
+    if (e.key === 'ArrowDown') {
+      e.preventDefault();
+      this.selectedIndex = this.selectedIndex < filtered.length - 1 ? this.selectedIndex + 1 : 0;
+      this.$nextTick(() => this.scrollToSelected());
+    } else if (e.key === 'ArrowUp') {
+      e.preventDefault();
+      this.selectedIndex = this.selectedIndex > 0 ? this.selectedIndex - 1 : filtered.length - 1;
+      this.$nextTick(() => this.scrollToSelected());
+    } else if (e.key === 'Enter' && this.selectedIndex >= 0) {
+      e.preventDefault();
+      this.selectOption(filtered[this.selectedIndex]);
+    } else if (e.key === 'Escape') {
+      this.showList = false;
+      this.selectedIndex = -1;
+    }
+  },
+  scrollToSelected() {
+    const container = this.$root.querySelector('.overflow-auto');
+    if (!container) return;
+    const items = container.querySelectorAll('li');
+    if (items[this.selectedIndex]) {
+      items[this.selectedIndex].scrollIntoView({ block: 'nearest' });
+    }
+  },
+  handleFocus() {
+    this.showList = true;
+    this.selectedIndex = -1;
+  }
+});
+
 window.autocompleter = function() {
   return {
+    ...autocompleterBase(),
     options: [],
-    search: '',
-    showList: false,
     parentVariable: '',
     parentVarGroup: '',
     dataSource: [],
+    init() {
+      this.$watch('search', () => { this.selectedIndex = -1; });
+    },
     setData(data) {
       this.dataSource = data;
       this.loadOptions();
@@ -16,12 +55,13 @@ window.autocompleter = function() {
         this.parentVarGroup = 'formData';
       }
       this.parentVariable = this.$el.getAttribute('data-parent-variable');
-
       this.options = this.dataSource;
       this.search = this[this.parentVarGroup] ? this[this.parentVarGroup][this.parentVariable] : '';
     },
     selectOption(option) {
       this.search = this[this.parentVarGroup][this.parentVariable] = option;
+      this.showList = false;
+      this.selectedIndex = -1;
     },
     get filteredOptions() {
       return this.options.filter(option => option.toLowerCase().includes(this.search.toLowerCase()));
@@ -34,21 +74,21 @@ window.autocompleter = function() {
 
 window.autocompleterUser = function() {
   return {
-    search: '',
-    showList: false,
+    ...autocompleterBase(),
     parentVariable: '',
     parentVariableUsername: '',
     parentVarGroup: '',
     dataSource: 'users',
     element: null,
-
+    init() {
+      this.$watch('search', () => { this.selectedIndex = -1; });
+    },
     setDataSource(dataSource) {
       this.dataSource = dataSource;
       this.loadOptions();
     },
     loadOptions() {
       this.element = this.$el;
-
       this.parentVarGroup = this.$el.getAttribute('data-parent-var-group');
       if(!this.parentVarGroup) {
         this.parentVarGroup = '';
@@ -81,8 +121,9 @@ window.autocompleterUser = function() {
           this[this.parentVarGroup][this.parentVariableUsername] = option.username;
         }
       }
-
       this.element.dispatchEvent(new Event('user-selected'));
+      this.showList = false;
+      this.selectedIndex = -1;
     },
     get filteredOptions() {
       if(this[this.dataSource] === undefined) {
@@ -98,17 +139,15 @@ window.autocompleterUser = function() {
 
 window.autocompleterIcon = function(dataSource) {
   return {
-    search: '',
-    showList: false,
+    ...autocompleterBase(),
     parentVariable: '',
     parentVarGroup: '',
     dataSource,
-
     init() {
       this.parentVarGroup = this.$el.getAttribute('data-parent-var-group');
       this.parentVariable = this.$el.getAttribute('data-parent-variable');
-
       this.loadOptions();
+      this.$watch('search', () => { this.selectedIndex = -1; });
     },
     loadOptions() {
       const selectedIcon = this.dataSource.find(itm => itm.url === this[this.parentVarGroup][this.parentVariable]);
@@ -117,6 +156,8 @@ window.autocompleterIcon = function(dataSource) {
     selectOption(option) {
       this.search = option.description;
       this[this.parentVarGroup][this.parentVariable] = option.url;
+      this.showList = false;
+      this.selectedIndex = -1;
     },
     get filteredOptions() {
       return this.dataSource
@@ -128,6 +169,8 @@ window.autocompleterIcon = function(dataSource) {
     clear() {
       this.search = '';
       this[this.parentVarGroup][this.parentVariable] = '';
+      this.showList = false;
+      this.selectedIndex = -1;
     },
     refresh() {
       this.loadOptions();
